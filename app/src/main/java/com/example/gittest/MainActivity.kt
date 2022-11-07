cf8540566036359f2a1f3c2467a4a5664f62d893
Gaurav <bamugadegaurav@gmail.com>
2022-11-07 22:11:57 +0530

    creating conflict 2 master

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -7,6 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        aisdjgasjkhd
+        println("creating conflict 1 master")
+        println("creating conflict 1 master")
+
     }
 }
